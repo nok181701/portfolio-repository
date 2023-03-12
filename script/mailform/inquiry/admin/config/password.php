99c1509f0e50880039e7d4d2aac11f61ca702033
祝井直樹 <nok181701@gmail.com>
2023-03-13 02:24:25 +0900

    1004：問い合わせフォーム改修

--- script/mailform/inquiry/admin/config/password.php
+++ script/mailform/inquiry/admin/config/password.php
@@ -2,7 +2,7 @@
 <?php
 define('ID', 'xs219310');
 define('PASSWORD', 'bf234f3e90cb14a91e1e9c2bffdf8518');
-define('PASSWORD2', '79b3494bee97c08d24151b0ce4d5776a');
+define('PASSWORD2', 'cea3afb494cfbde95d4f195ae4c1a62d');
 define('MAILADDRESS', 'mailform@sv14560.xserver.jp');
 define('RECEIVE_MAILADDRESS', 'nok181701@gmail.com');
 ?>
